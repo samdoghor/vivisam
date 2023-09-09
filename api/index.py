22cac3ca6d8ca40073460947ce7f09588fa0a9c6
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-09-09 20:38:36 +0100

    fix

--- api/index.py
+++ api/index.py
@@ -1,133 +1,17 @@
-"""
-This Module defines the routes of the backend for Vivirgros and Samuel Doghor
-Website
-"""
-
-
-# imports
-
-
-import smtplib
-
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from flask_migrate import Migrate
-
-from .config import (DEBUG, EMAIL_ADDRESS, EMAIL_HOST, EMAIL_PASSWORD,
-                     SECRET_KEY, SQLALCHEMY_DATABASE_URI,
-                     SQLALCHEMY_MODIFICATIONS_TRACKS)
-from .models import db
-
-# configurations
+from flask import Flask
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = SECRET_KEY
-
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_MODIFICATIONS_TRACKS'] = SQLALCHEMY_MODIFICATIONS_TRACKS  # noqa
-
-debug_mode = DEBUG
-
-db.init_app(app)
-db.app = (app)
-migrate = Migrate(app, db)
-
-CORS(app, resources={
-     r"/contact": {"origins": ["http://localhost:5173",
-                               "https://vivirgros.com"]}})
-
-
-# routes
 
 
 @app.route('/')
 def home():
-    """ The funtion returns a 200 Ok to show app is running """
-
-    data = {
-        'Status': '200 Ok',
-        'Message': 'App is running'
-    }
-
-    if data:
-        try:
-            return jsonify({
-                'Status': '200 Ok',
-                'Message': 'App is runnung',
-            }), 200
-        except Exception:
-            return jsonify({
-                'Status': '500 Internal Server Error',
-                'Message': 'App is not runnung',
-            }), 500
-
-    else:
-        return jsonify({
-            'Status': '500 Internal Server Error',
-            'Message': 'App is not runnung',
-        }), 500
+    return 'Hello, World!'
 
 
-@app.route('/contact', methods=['GET', 'POST'])
-def send_mail():
-    """ This function sends mail in contact page for Vivirgros """
-
-    data = request.get_json()
-
-    if data:
-        try:
-            company_name = data["companyName"]
-            your_name = data["yourName"]
-            phone_number = data["phoneNumber"]
-            email_address = data["emailAddress"]
-            project_details = data["projectDetails"]
-
-            mail_subject = "A Message from Vivirgros Contact Page"
-            mail_message = f"""\
-            This message is from Vivirgros Contact Page
-
-            Message Details Below:
-
-                Company Name: {company_name}
-
-                Client Name: {your_name}
-
-                Mobile Number: {phone_number}
-
-                Email Address: {email_address}
-
-                Message: {project_details}"""
-
-            email = EMAIL_ADDRESS
-            password = EMAIL_PASSWORD
-            to = EMAIL_ADDRESS
-
-            with smtplib.SMTP_SSL('mail.'+EMAIL_HOST, 465) as smtp:
-
-                smtp.login(email, password)
-
-                subject = mail_subject
-                body = mail_message
-
-                msg = f"Subject: {subject}\n\n{body}"
-
-                smtp.sendmail(email, to, msg)
-
-            return jsonify({
-                'Message': 'Sent Successfully',
-            }), 200
-
-        except Exception:
-            print(Exception)
-            # Return an error response as JSON
-            return jsonify({'Message': 'Failed to send'}), 500
-
-    else:
-        print(Exception)
-        # Return an error response as JSON
-        return jsonify({'Message': 'Failed to send'}), 500
+@app.route('/about')
+def about():
+    return 'About'
 
 
-if __name__ == '__main__':
-    app.run(debug=debug_mode)
+if __name__ == "__main__":
+    app.run()
