0bc863e49f4d687e17ac0f12d93d46b9b754e3aa
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-09-09 20:44:15 +0100

    fix

--- api/index.py
+++ api/index.py
@@ -1,6 +1,22 @@
+"""
+This Module defines the routes of the backend for Vivirgros and Samuel Doghor
+Website
+"""
+
+
 from flask import Flask
+from flask_migrate import Migrate
+
+from .models import db
+
+# configurations
+
 
 app = Flask(__name__)
+
+db.init_app(app)
+db.app = (app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
