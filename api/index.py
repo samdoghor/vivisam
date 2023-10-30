d57c9a2b99919f44c60a8353e4b2f3dc16068cca
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-10-30 18:53:31 +0100

    feat: blog CRUD

--- api/index.py
+++ api/index.py
@@ -9,9 +9,11 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 
 from .config import (EMAIL_ADDRESS, EMAIL_HOST, EMAIL_PASSWORD, SECRET_KEY, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MODIFICATIONS_TRACKS)  # noqa
-from .errors import BadRequest, DataNotFound, TooManyRequest
+from .errors import (BadRequest, DataNotFound, TooManyRequest,
+                     Forbidden, Conflict, InternalServerError)
 
-from .models import db
+from .models import (db, EmailListModel, AuthorModel,
+                     BlogModel, BlogContentModel, BlogImageModel)
 
 # configurations
 
@@ -29,7 +31,8 @@ migrate = Migrate(app, db)
 CORS(app, resources={
      r"/contact": {"origins": ["https://www.vivirgros.com",
                                "https://vivirgros.com",
-                               "vivirgros.com"]}})
+                               "vivirgros.com", "http://localhost:5173",
+                               "localhost:5173"]}})
 
 # routes
 
@@ -42,6 +45,446 @@ def home():
         'code': 200,
         'message': 'The application is running'
     })
+
+
+""" Authors """
+# create new author
+
+
+@app.route('/new_author', methods=['POST'])
+def new_author(first_name, last_name, email_address, profile_picture, password):  # noqa
+    """ This function defines a route to create a new author """
+
+    try:
+        authors = AuthorModel.query.filter_by(
+            email_address=email_address).first()
+
+        if not authors:
+            new_author = AuthorModel(
+                first_name=first_name,
+                last_name=last_name,
+                email_address=email_address,
+                profile_picture=profile_picture
+            )
+            new_author.set_password(password)
+
+            db.session.add(new_author)
+            db.session.commit()
+
+            return jsonify({
+                'Message': 'Author Created Successfully',
+            }), 200
+
+        else:
+            return jsonify({
+                'Message': 'Author Already Exist'
+            })
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# get one author
+
+
+@app.route('/author/<int:id>', methods=['GET'])
+def author(id):
+    """ This function defines a route to display an author info """
+
+    try:
+        authors = AuthorModel.query.filter_by(id=id).first()
+
+        if not authors:
+
+            return jsonify({
+                'Message': f'Author with id {id} was not found',
+            }), 200
+
+        else:
+            data = {
+                'id': authors.id,
+                'first_name': authors.first_name,
+                'last_name': authors.last_name,
+                'email_address': authors.email_address,
+                'profile_picture': authors.profile_picture
+            }
+
+            return jsonify({
+                'Message': f'Author with id {id} was found',
+                'Author': data
+            })
+
+    except DataNotFound as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# update one author
+
+@app.route('/author/<int:id>', methods=['PUT'])
+def update_author(id, first_name, last_name, email_address, profile_picture, password):  # noqa
+    """ This function defines a route to update an existing author """
+
+    try:
+        author = AuthorModel.query.filter_by(id=id).first()
+
+        if author:
+            update_author = AuthorModel(
+                first_name=first_name,
+                last_name=last_name,
+                email_address=email_address,
+                profile_picture=profile_picture
+            )
+            update_author.set_password(password)
+
+            db.session.commit()
+
+            return jsonify({
+                'Message': 'Author Updated Successfully',
+            }), 200
+
+        else:
+            return jsonify({
+                'Message': 'Author Update Failed'
+            })
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# delete one author
+
+
+@app.route('/author/<int:id>', methods=['DELETE'])
+def delete_author(id):  # noqa
+    """ This function defines a route to delte an author """
+
+    try:
+        authors = AuthorModel.query.filter_by(id=id).first()
+
+        if not authors:
+
+            return jsonify({
+                'Message': f'Author with id {id} was not found',
+            }), 200
+
+        else:
+            db.session.delete(authors)
+            db.session.commit()
+
+            return jsonify({
+                'Message': f'Author with id {id} was found and was deleted',
+            })
+
+    except DataNotFound as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+""" Blogs """
+# create new blog
+
+
+@app.route('/new_blog', methods=['POST'])
+def new_blog(id, title, feature_image, content):  # noqa
+    """ This function defines a route to create a new blog """
+
+    try:
+        new_blog = BlogModel(
+            title=title,
+            feature_image=feature_image
+        )
+        db.session.add(new_blog)
+
+        new_content = BlogContentModel(
+            blog_id=BlogModel.query.filter_by(id=id).first(),
+            content=content
+        )
+        db.session.add(new_content)
+
+        new_image = BlogImageModel(
+            blog_id=BlogModel.query.filter_by(id=id).first(),
+            image=content
+        )
+        db.session.add(new_image)
+
+        db.session.commit()
+
+        return jsonify({
+            'Message': 'Blog Created Successfully',
+        }), 200
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# get one blog
+
+
+@app.route('/blog/<int:id>', methods=['GET'])
+def blog(id):
+    """ This function defines a route to display a blog """
+
+    try:
+        authors = AuthorModel.query.filter_by(id=id).first()
+
+        if not authors:
+
+            return jsonify({
+                'Message': f'Author with id {id} was not found',
+            }), 200
+
+        else:
+            data = {
+                'id': authors.id,
+                'first_name': authors.first_name,
+                'last_name': authors.last_name,
+                'email_address': authors.email_address,
+                'profile_picture': authors.profile_picture
+            }
+
+            return jsonify({
+                'Message': f'Author with id {id} was found',
+                'Author': data
+            })
+
+    except DataNotFound as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# update one blog
+
+@app.route('/blog/<int:id>', methods=['PUT'])
+def update_blog(id, first_name, last_name, email_address, profile_picture, password):  # noqa
+    """ This function defines a route to update an existing blog """
+
+    try:
+        author = AuthorModel.query.filter_by(id=id).first()
+
+        if author:
+            update_author = AuthorModel(
+                first_name=first_name,
+                last_name=last_name,
+                email_address=email_address,
+                profile_picture=profile_picture
+            )
+            update_author.set_password(password)
+
+            db.session.commit()
+
+            return jsonify({
+                'Message': 'Author Updated Successfully',
+            }), 200
+
+        else:
+            return jsonify({
+                'Message': 'Author Update Failed'
+            })
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+# delete one blog
+
+
+@app.route('/blog/<int:id>', methods=['DELETE'])
+def deleteblog(id):  # noqa
+    """ This function defines a route to delete a blog """
+
+    try:
+        authors = AuthorModel.query.filter_by(id=id).first()
+
+        if not authors:
+
+            return jsonify({
+                'Message': f'Author with id {id} was not found',
+            }), 200
+
+        else:
+            db.session.delete(authors)
+            db.session.commit()
+
+            return jsonify({
+                'Message': f'Author with id {id} was found and was deleted',
+            })
+
+    except DataNotFound as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Forbidden as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except Conflict as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+    except InternalServerError as e:
+        return {
+            'Code': e.code,
+            'Type': e.type,
+            'Message': e.message
+        }
+
+
+""" Contact """
+# send a contact message to vivirgros and save details in contact list
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -88,6 +531,21 @@ def send_mail():
                 msg = f"Subject: {subject}\n\n{body}"
 
                 smtp.sendmail(email, to_email, msg)
+
+            # add to customer list
+
+            customer = EmailListModel.query.filter_by(
+                email_address=email_address).first()
+
+            if not customer:
+                new_customer = EmailListModel(
+                    company_name=company_name,
+                    customer_name=your_name,
+                    email_address=email_address,
+                    phone_number=phone_number,
+                )
+                db.session.add(new_customer)
+                db.session.commit()
 
             return jsonify({
                 'Message': 'Sent Successfully',
--- api/models.py
+++ api/models.py
@@ -6,15 +6,15 @@ Website
 
 from datetime import datetime
 
-# from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # db initialisation
 
 db = SQLAlchemy()
 
 
-class Author(db.Model):
+class AuthorModel(db.Model):
     """ Author Model """
     __tablename__ = "authors"
 
@@ -23,44 +23,87 @@ class Author(db.Model):
     last_name = db.Column(db.String(), nullable=False)
     email_address = db.Column(db.String(), unique=True, nullable=False)
     profile_picture = db.Column(db.String(), nullable=True)
+    password = db.Column(db.Integer(), nullable=False)
+
+    # relationships
+
+    blogs = db.relationship("BlogModel", backref="authors", lazy=True)
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-class Blog(db.Model):
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class BlogModel(db.Model):
     """ Blog Model """
     __tablename__ = "blogs"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), nullable=False)
-    content = db.Column(db.String(), nullable=False)
-    thumbnail = db.Column(db.String())
+    feature_image = db.Column(db.String(), nullable=False)
+
+    # foreign_keys
+
+    author_id = db.Column(db.Integer, db.ForeignKey(
+        'authors.id'), nullable=False, unique=True)
+
+    # relationships
+
+    blog_contents = db.relationship(
+        "BlogContentModel", backref="blogs", lazy=True)
+    blog_images = db.relationship(
+        "BlogImageModel", backref="blogs", lazy=True)
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
 
-class Blog_Image(db.Model):
+class BlogContentModel(db.Model):
+    """ Blog Content Model """
+    __tablename__ = "blog_contents"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(), nullable=False)
+
+    # foreign_keys
+
+    blog_id = db.Column(db.Integer, db.ForeignKey(
+        'blogs.id'), nullable=False, unique=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+
+class BlogImageModel(db.Model):
     """ Blog Image Model """
     __tablename__ = "blog_images"
 
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(), nullable=False)
 
+    # foreign_keys
+
+    blod_id = db.Column(db.Integer, db.ForeignKey(
+        'blogs.id'), nullable=False, unique=True)
+
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
 
-class Email_list(db.Model):
+class EmailListModel(db.Model):
     """ Email List Model """
     __tablename__ = "email_lists"
 
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(), nullable=False)
     customer_name = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
-    phone_name = db.Column(db.String(), nullable=False)
+    email_address = db.Column(db.String(), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
