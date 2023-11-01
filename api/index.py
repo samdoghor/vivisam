60448ad3d535fd4f5e6f49d8a0ab53cd6755f7f1
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-11-01 23:27:13 +0100

    hotfix: n-CORS-6

--- api/index.py
+++ api/index.py
@@ -33,8 +33,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-allowed_origins = ["https://www.vivirgros.com/",
-                   "https://vivirgros.com/", "vivirgros.com/"]
+allowed_origins = ["https://www.vivirgros.com",
+                   "https://vivirgros.com", "vivirgros.com"]
 
 CORS(app, resources={r"/contact": {"origins": allowed_origins}})
 
